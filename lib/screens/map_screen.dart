045c0ebdfaf73c041ef3225c5868389a9da3727e
Lee seohuyn <leesophy0805@gmail.com>
2025-01-27 02:43:57 +0900

    [기능추가] 지도 화면 추가 : feat : 현재 내 위치에서 지도 카메라 초기화 #1

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -3,6 +3,8 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -12,10 +14,33 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  NLatLng? _currentPosition;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    _permission();
+  }
+
+  void _permission() async {
+    var requestStatus = await Permission.location.request();
+    var status = await Permission.location.status;
+    if (requestStatus.isPermanentlyDenied || status.isPermanentlyDenied) {
+      openAppSettings();
+    } else {
+      await _getCurrentPosition();
+    }
+  }
+
+  Future<void> _getCurrentPosition() async {
+    try {
+      final position = await Geolocator.getCurrentPosition();
+      setState(() {
+        _currentPosition = NLatLng(position.latitude, position.longitude);
+      });
+    } catch (e) {
+      log('Error getting location: $e');
+    }
   }
 
   @override
@@ -25,19 +50,34 @@ class _MapScreenState extends State<MapScreen> {
 
     return MaterialApp(
       home: Scaffold(
-        body: NaverMap(
-          options: const NaverMapViewOptions(
-            indoorEnable: true, // 실내 맵 사용 가능 여부 설정
-            locationButtonEnable: false, // 위치 버튼 표시 여부 설정
-            consumeSymbolTapEvents: false, // 심볼 탭 이벤트 소비 여부 설정
-          ),
-          onMapReady: (controller) async {
-            // 지도 준비 완료 시 호출되는 콜백 함수
-            mapControllerCompleter
-                .complete(controller); // Completer에 지도 컨트롤러 완료 신호 전송
-            log("onMapReady", name: "onMapReady");
-          },
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: const Text('동네 인증하기'),
         ),
+        body: // 현재 위치를 가져오기 전이라면 로딩 화면 표시
+            _currentPosition == null
+                ? const Center(
+                    child: CircularProgressIndicator(), // 로딩 스피너
+                  )
+                : NaverMap(
+                    options: NaverMapViewOptions(
+                      initialCameraPosition: NCameraPosition(
+                        target: _currentPosition!, // null이 아님을 보장
+                        zoom: 15,
+                      ),
+                      indoorEnable: true, // 실내 맵 사용 가능 여부 설정
+                      locationButtonEnable: true, // 위치 버튼 표시 여부 설정
+                      consumeSymbolTapEvents: false, // 심볼 탭 이벤트 소비 여부 설정
+                    ),
+                    onMapReady: (controller) async {
+                      mapControllerCompleter.complete(controller);
+                      log("onMapReady", name: "onMapReady");
+
+                      // 위치 추적 모드 활성화
+                      await controller.setLocationTrackingMode(
+                          NLocationTrackingMode.follow);
+                    },
+                  ),
       ),
     );
   }
