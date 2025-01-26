3df6f162cf0a3a30fcc3db228f7f68c18a01aa5c
Lee seohuyn <leesophy0805@gmail.com>
2025-01-27 00:13:07 +0900

    [기능추가] 지도 화면 추가 : feat : 지도 화면 불러오기 구현 #1

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // NaverMapController 객체의 비동기 작업 완료를 나타내는 Completer 생성
+    final Completer<NaverMapController> mapControllerCompleter = Completer();
+
+    return MaterialApp(
+      home: Scaffold(
+        body: NaverMap(
+          options: const NaverMapViewOptions(
+            indoorEnable: true, // 실내 맵 사용 가능 여부 설정
+            locationButtonEnable: false, // 위치 버튼 표시 여부 설정
+            consumeSymbolTapEvents: false, // 심볼 탭 이벤트 소비 여부 설정
+          ),
+          onMapReady: (controller) async {
+            // 지도 준비 완료 시 호출되는 콜백 함수
+            mapControllerCompleter
+                .complete(controller); // Completer에 지도 컨트롤러 완료 신호 전송
+            log("onMapReady", name: "onMapReady");
+          },
+        ),
+      ),
+    );
+  }
+}
