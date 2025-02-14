5175ad59152f2e1931fedaa85368fd4f6c40c80e
Lee seohuyn <leesophy0805@gmail.com>
2025-02-15 00:12:30 +0900

    카카오 소셜 로그인 기능 추가 : refactor : secureStorage 저장 키 값 enum으로 저장  https://github.com/TEAM-ROMROM/RomRom-FE/issues/3

--- lib/services/token_manage_service.dart
+++ lib/services/token_manage_service.dart
@@ -1,29 +1,30 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:romrom_fe/main.dart';
 import 'package:http/http.dart' as http;
+import 'package:romrom_fe/models/storage_keys.dart';
 
 const storage = FlutterSecureStorage();
 
 /// 토큰 저장
 Future<void> saveTokens(String accessToken, String refreshToken) async {
-  await storage.write(key: 'access_token', value: accessToken);
-  await storage.write(key: 'refresh_token', value: refreshToken);
+  await storage.write(key: StorageKeys.accessToken.name, value: accessToken);
+  await storage.write(key: StorageKeys.refreshToken.name, value: refreshToken);
 }
 
 /// 액세스 토큰 불러오기
 Future<String?> getAccessToken() async {
-  return await storage.read(key: 'access_token');
+  return await storage.read(key: StorageKeys.accessToken.name);
 }
 
 /// 리프레시 토큰 불러오기
 Future<String?> getRefreshToken() async {
-  return await storage.read(key: 'refresh_token');
+  return await storage.read(key: StorageKeys.refreshToken.name);
 }
 
 /// 토큰 삭제
 Future<void> deleteTokens() async {
-  await storage.delete(key: 'access_token');
-  await storage.delete(key: 'refresh_token');
+  await storage.delete(key: StorageKeys.accessToken.name);
+  await storage.delete(key: StorageKeys.refreshToken.name);
 }
 
 /// 엑세스 토큰 재발급
