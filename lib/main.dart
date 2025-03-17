1e4ef807dd2e8fbbc70c6720832ecf808b326c37
Lee seohuyn <leesophy0805@gmail.com>
2025-03-17 23:40:45 +0900

    공통적인 파일이름 통합 필요, API 코드 백엔드 구조와 동기화 필요 : refactor : api 관련 파일 이름 형식 변경 https://github.com/TEAM-ROMROM/RomRom-FE/issues/35

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:romrom_fe/models/app_theme.dart';
 import 'package:romrom_fe/screens/home_screen.dart';
 import 'package:romrom_fe/screens/login_screen.dart';
+import 'package:romrom_fe/services/api/auth_api.dart';
 import 'package:romrom_fe/services/app_initializer.dart';
 import 'package:romrom_fe/services/token_manager.dart';
 
