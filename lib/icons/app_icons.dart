eef341817cdcb49490964105f2c1068f653c08f4
Lee seohuyn <leesophy0805@gmail.com>
2025-04-03 03:13:27 +0900

    로그인 버튼 정렬 Figma 명세와 다른 문제 : feat : 로그인 화면 icon 추가 https://github.com/TEAM-ROMROM/RomRom-FE/issues/54

---
+++ lib/icons/app_icons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class AppIcons {
+  AppIcons._();
+
+  static const _kFontFam = 'AppIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData tempLogo =
+      IconData(0xe810, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
