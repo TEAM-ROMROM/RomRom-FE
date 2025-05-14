222f60b81a21216c2aa4236dc5f948f514014441
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-05-14 21:04:31 +0900

    네비게이션 바 컴포넌트 추가 : fix : flutter doctor 오류 수정 https://github.com/TEAM-ROMROM/RomRom-FE/issues/76

--- lib/widgets/custom_bottom_navigation_bar.dart
+++ lib/widgets/custom_bottom_navigation_bar.dart
@@ -9,10 +9,10 @@ class CustomBottomNavigationBar extends StatelessWidget {
   final Function(int) onTap;
 
   const CustomBottomNavigationBar({
-    Key? key,
+    super.key,
     required this.selectedIndex,
     required this.onTap,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
