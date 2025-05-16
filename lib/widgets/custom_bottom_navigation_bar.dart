e5e1cdddcfc07c151c4e4eee98e3aa7e1693838c
Lee seohuyn <leesophy0805@gmail.com>
2025-05-16 14:16:51 +0900

    네비게이션 바 컴포넌트 추가 : fix :config.json 파일에서  home 아이콘 파일명 영어로 수정https://github.com/TEAM-ROMROM/RomRom-FE/issues/76

--- lib/widgets/custom_bottom_navigation_bar.dart
+++ lib/widgets/custom_bottom_navigation_bar.dart
@@ -31,42 +31,18 @@ class CustomBottomNavigationBar extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          _buildNavItem(
-              context,
-              0,
-              AppIcons.home,
-              '홈'
-          ),
-          _buildNavItem(
-              context,
-              1,
-              AppIcons.requestManagement,
-              '요청 관리'
-          ),
-          _buildNavItem(
-              context,
-              2,
-              AppIcons.register,
-              '등록'
-          ),
-          _buildNavItem(
-              context,
-              3,
-              AppIcons.chat,
-              '채팅'
-          ),
-          _buildNavItem(
-              context,
-              4,
-              AppIcons.myPage,
-              '마이페이지'
-          ),
+          _buildNavItem(context, 0, AppIcons.home, '홈'),
+          _buildNavItem(context, 1, AppIcons.requestManagement, '요청 관리'),
+          _buildNavItem(context, 2, AppIcons.register, '등록'),
+          _buildNavItem(context, 3, AppIcons.chat, '채팅'),
+          _buildNavItem(context, 4, AppIcons.myPage, '마이페이지'),
         ],
       ),
     );
   }
 
-  Widget _buildNavItem(BuildContext context, int index, IconData icon, String label) {
+  Widget _buildNavItem(
+      BuildContext context, int index, IconData icon, String label) {
     final bool isSelected = selectedIndex == index;
 
     return InkWell(
@@ -79,7 +55,9 @@ class CustomBottomNavigationBar extends StatelessWidget {
             // 아이콘 (선택됨: 흰색, 선택 안됨: 회색)
             Icon(
               icon,
-              color: isSelected ? Colors.white : AppColors.bottomNavigationDisableIcon,
+              color: isSelected
+                  ? Colors.white
+                  : AppColors.bottomNavigationDisableIcon,
               size: 24.sp,
             ),
             SizedBox(height: 9.h), // 아이콘과 텍스트 사이 간격
@@ -87,7 +65,9 @@ class CustomBottomNavigationBar extends StatelessWidget {
             Text(
               label,
               style: CustomTextStyles.p2.copyWith(
-                color: isSelected ? Colors.white : AppColors.bottomNavigationDisableIcon,
+                color: isSelected
+                    ? Colors.white
+                    : AppColors.bottomNavigationDisableIcon,
               ),
               textAlign: TextAlign.center,
             ),
