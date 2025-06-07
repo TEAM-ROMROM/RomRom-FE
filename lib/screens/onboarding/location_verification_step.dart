ae4d30823e2d1d21b6eaedfb0d021c921450ae4b
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-06-07 11:55:51 +0900

    온보딩 UI 변경 헤더 통일화 필요 : refactor : location_service.dart 로직 분리, try catch 에러코드 안정성 추가 https://github.com/TEAM-ROMROM/RomRom-FE/issues/100

--- lib/screens/onboarding/location_verification_step.dart
+++ lib/screens/onboarding/location_verification_step.dart
@@ -1,20 +1,14 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart' as http;
-import 'package:permission_handler/permission_handler.dart';
 import 'package:romrom_fe/icons/app_icons.dart';
-import 'package:romrom_fe/models/apis/responses/naver_address_response.dart';
 import 'package:romrom_fe/models/app_colors.dart';
 import 'package:romrom_fe/models/app_theme.dart';
-import 'package:romrom_fe/models/app_urls.dart';
 import 'package:romrom_fe/services/apis/member_api.dart';
+import 'package:romrom_fe/services/location_service.dart';
 
 class LocationVerificationStep extends StatefulWidget {
   final VoidCallback onNext;
@@ -27,6 +21,7 @@ class LocationVerificationStep extends StatefulWidget {
 }
 
 class _LocationVerificationStepState extends State<LocationVerificationStep> {
+  final _locationService = LocationService();
   NLatLng? _currentPosition;
   String currentAdress = '';
   String siDo = '';
@@ -38,7 +33,7 @@ class _LocationVerificationStepState extends State<LocationVerificationStep> {
   @override
   void initState() {
     super.initState();
-    _permission();
+    _initializeLocation();
   }
 
   @override
@@ -221,64 +216,36 @@ class _LocationVerificationStepState extends State<LocationVerificationStep> {
     }
   }
 
-  Future<void> _permission() async {
-    var requestStatus = await Permission.location.request();
-    var status = await Permission.location.status;
+  // 위치 초기화 통합 메서드
+  Future<void> _initializeLocation() async {
+    final hasPermission = await _locationService.requestPermission();
+    if (!hasPermission) return;
 
-    if (requestStatus.isPermanentlyDenied || status.isPermanentlyDenied) {
-      openAppSettings();
-    } else {
-      await _getCurrentPosition();
-    }
-  }
-
-  Future<void> _getCurrentPosition() async {
-    try {
-      final position = await Geolocator.getCurrentPosition();
+    final position = await _locationService.getCurrentPosition();
+    if (position != null) {
       setState(() {
-        _currentPosition = NLatLng(position.latitude, position.longitude);
+        _currentPosition = _locationService.positionToLatLng(position);
       });
-    } catch (e) {
-      log('Error getting location: $e');
     }
   }
 
-  Future<void> getAddressByNaverApi(NLatLng position) async {
-    const String naverReverseGeoCodeApiUrl = AppUrls.naverReverseGeoCodeApiUrl;
-    String coords = "${position.longitude},${position.latitude}";
-    const String orders = "legalcode";
-    const String output = "json";
+  // 주소 정보 로드 메서드
+  Future<void> _loadAddressInfo(NLatLng position) async {
+    final addressInfo = await _locationService.getAddressFromCoordinates(position);
 
-    try {
-      final requestUrl =
-          "$naverReverseGeoCodeApiUrl?coords=$coords&orders=$orders&output=$output";
-
-      final response = await http.get(
-        Uri.parse(requestUrl),
-        headers: {
-          "X-NCP-APIGW-API-KEY-ID": dotenv.get('NMF_CLIENT_ID'),
-          "X-NCP-APIGW-API-KEY": dotenv.get('NMF_CLIENT_SECRET'),
-        },
-      );
-
-      if (response.statusCode == 200) {
-        final NaverAddressResponse addressData =
-            NaverAddressResponse.fromJson(json.decode(response.body));
-
-        if (addressData.results.isNotEmpty) {
-          final region = addressData.results[0].region;
-
-          setState(() {
-            siDo = region.area1.name;
-            siGunGu = region.area2.name;
-            eupMyoenDong = region.area3.name;
-            ri = region.area4.name.isNotEmpty ? region.area4.name : null;
-            currentAdress = eupMyoenDong;
-          });
-        }
-      }
-    } catch (e) {
-      log("주소 요청 중 오류 발생: $e");
+    if (addressInfo != null) {
+      setState(() {
+        siDo = addressInfo.siDo;
+        siGunGu = addressInfo.siGunGu;
+        eupMyoenDong = addressInfo.eupMyoenDong;
+        ri = addressInfo.ri;
+        currentAdress = addressInfo.currentAddress;
+      });
     }
+  }
+
+  // NaverMap에서 주소 가져오는 메서드를 새 메서드로 호출
+  Future<void> getAddressByNaverApi(NLatLng position) async {
+    await _loadAddressInfo(position);
   }
 }
