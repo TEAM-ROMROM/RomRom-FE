e93dbf7bcbf3bf72afc76e360330436ad85442f1
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-06-10 20:12:15 +0900

    서비스 이용약관 페이지 추가 : refactor : import 삭제 https://github.com/TEAM-ROMROM/RomRom-FE/issues/108

--- lib/screens/onboarding/term_agreement_step.dart
+++ lib/screens/onboarding/term_agreement_step.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:romrom_fe/data/terms_data.dart';
 import 'package:romrom_fe/enums/navigation_types.dart';
 import 'package:romrom_fe/enums/term_type.dart';
