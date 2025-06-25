ce84af33626d1b848f88ed205770ac9b711e6ce6
Lee seohuyn <leesophy0805@gmail.com>
2025-06-26 01:57:21 +0900

    물건 등록 화면 추가 : feat : 등록 tab 화면에 물품등록 임시 버튼 추가, 색상 추가 https://github.com/TEAM-ROMROM/RomRom-FE/issues/126

--- lib/screens/register_tab_screen.dart
+++ lib/screens/register_tab_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:romrom_fe/models/app_theme.dart';
+import 'package:romrom_fe/models/app_colors.dart';
+import 'package:romrom_fe/screens/item_register_screen.dart';
 
 class RegisterTabScreen extends StatelessWidget {
   const RegisterTabScreen({super.key});
@@ -7,9 +8,21 @@ class RegisterTabScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        '등록 페이지 내용',
-        style: CustomTextStyles.h3,
+      child: TextButton.icon(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const ItemRegisterScreen()),
+          );
+        },
+        label: const Text(
+          '등록하기',
+          style: TextStyle(color: AppColors.textColorBlack),
+        ),
+        icon: const Icon(Icons.add),
+        style: ButtonStyle(
+          backgroundColor: WidgetStateProperty.all(AppColors.primaryYellow),
+          iconColor: WidgetStateProperty.all(AppColors.primaryBlack),
+        ),
       ),
     );
   }
