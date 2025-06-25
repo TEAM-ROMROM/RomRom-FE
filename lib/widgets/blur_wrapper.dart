e03a321eff0795b39aa7a0e22cf22f770162855e
Lee seohuyn <leesophy0805@gmail.com>
2025-06-26 00:37:07 +0900

    등록 유도 페이지 추가 : feat : 블러 위젯 구현https://github.com/TEAM-ROMROM/RomRom-FE/issues/123

---
+++ lib/widgets/blur_wrapper.dart
@@ -0,0 +1,26 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+/// child를 블러처리하는 위젯
+class BlurWrapper extends StatelessWidget {
+  final bool enabled;
+  final double sigma; // 블러 강도
+  final Widget child;
+
+  const BlurWrapper({
+    super.key,
+    required this.enabled,
+    this.sigma = 8,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (!enabled) return child;
+    return ImageFiltered(
+      imageFilter: ImageFilter.blur(sigmaX: sigma, sigmaY: sigma),
+      child: child,
+    );
+  }
+}
