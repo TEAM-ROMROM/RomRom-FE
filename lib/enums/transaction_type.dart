477eacc859504e56cfb348afec3937345203d7d5
Lee seohuyn <leesophy0805@gmail.com>
2025-06-27 18:39:09 +0900

    물건 등록 화면 추가 : refactor : 거래 유형 순서 변경 https://github.com/TEAM-ROMROM/RomRom-FE/issues/126

--- lib/enums/transaction_type.dart
+++ lib/enums/transaction_type.dart
@@ -1,8 +1,8 @@
 /// 거래 유형
 enum TransactionType {
+  additionalCharge(name: '추가금'),
   directDeal(name: '직거래'),
-  delivery(name: '택배'),
-  additionalCharge(name: '추가금');
+  delivery(name: '택배');
 
   final String name;
 
