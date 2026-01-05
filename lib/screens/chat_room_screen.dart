d66f9edc42f2e0338c5775897d90c8772d62617e
Lee seohuyn <leesophy0805@gmail.com>
2026-01-05 17:03:38 +0900

    폰트 사이즈 up : feat : 채팅 폰트14, 입력창 폰트 12로 수정 https://github.com/TEAM-ROMROM/RomRom-FE/issues/393

--- lib/screens/chat_room_screen.dart
+++ lib/screens/chat_room_screen.dart
@@ -593,7 +593,7 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
                   ),
                   child: Text(
                     message.content ?? '',
-                    style: CustomTextStyles.p3.copyWith(
+                    style: CustomTextStyles.p2.copyWith(
                       color: AppColors.textColorWhite,
                       fontWeight: FontWeight.w400,
                       height: 1.2,
@@ -605,7 +605,7 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
                   Text(
                     formatMessageTime(message.createdDate),
                     style: CustomTextStyles.p3.copyWith(
-                      fontSize: 10.sp,
+                      fontSize: 12.sp,
                       color: AppColors.opacity50White,
                       fontWeight: FontWeight.w400,
                     ),
@@ -616,7 +616,7 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
                   Text(
                     formatMessageTime(message.createdDate),
                     style: CustomTextStyles.p3.copyWith(
-                      fontSize: 10.sp,
+                      fontSize: 12.sp,
                       color: AppColors.opacity50White,
                       fontWeight: FontWeight.w400,
                     ),
@@ -635,7 +635,7 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
                   ),
                   child: Text(
                     message.content ?? '',
-                    style: CustomTextStyles.p3.copyWith(
+                    style: CustomTextStyles.p2.copyWith(
                       color: AppColors.textColorBlack,
                       fontWeight: FontWeight.w400,
                       height: 1.2,
