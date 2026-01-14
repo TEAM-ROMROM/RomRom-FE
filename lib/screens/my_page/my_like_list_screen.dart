f568295721340d37050dfeffb9f9a2b18bcbf8b0
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2026-01-15 00:27:04 +0900

    개선된 요청하기 페이지 및 페이지 라우팅 개발 필요 : fix : 백엔드 ENUM으로 교체 (잘못수정했었음) https://github.com/TEAM-ROMROM/RomRom-FE/issues/414

--- lib/screens/my_page/my_like_list_screen.dart
+++ lib/screens/my_page/my_like_list_screen.dart
@@ -110,7 +110,7 @@ class _MyLikeListScreenState extends State<MyLikeListScreen> {
       final d = details[index];
 
       // 카테고리/상태/옵션 매핑
-      ItemCondition cond = ItemCondition.newItem;
+      ItemCondition cond = ItemCondition.sealed;
       try {
         cond = item_condition_enum.ItemCondition.values.firstWhere(
           (e) => e.serverName == d.itemCondition,
