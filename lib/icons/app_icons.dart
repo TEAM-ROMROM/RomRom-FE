d74a4eac9343c2ea32e6cc3e42ca5b57362cb713
Lee seohuyn <leesophy0805@gmail.com>
2026-01-19 17:18:54 +0900

    컴포넌트 수정 : fix : 더보기 dotsVertical 아이콘 수정 https://github.com/TEAM-ROMROM/RomRom-FE/issues/423

--- lib/icons/app_icons.dart
+++ lib/icons/app_icons.dart
@@ -11,6 +11,7 @@ class AppIcons {
   static const IconData addItemPlus = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData information = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData warning = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData dotsVertical = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData myPage = IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData register = IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData home = IconData(0xe80b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
@@ -22,7 +23,6 @@ class AppIcons {
   static const IconData profilelikecount = IconData(0xe817, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData cancel = IconData(0xe820, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData detailView = IconData(0xe821, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData dotsVertical = IconData(0xe822, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData itemRegisterHeart = IconData(0xe823, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData location = IconData(0xe824, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData navigateBefore = IconData(0xe825, fontFamily: _kFontFam, fontPackage: _kFontPkg);
