4389e41e39b7a747ec49e5359c479120928203b4
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2026-02-01 16:32:32 +0900

    Common Toast 디자인 변경 및 다중 토스트 알림시 겹침문제 개선 필요 : fix : error info success toast로 정상 표시되게 수정 https://github.com/TEAM-ROMROM/RomRom-FE/issues/469

--- lib/widgets/home_feed_item_widget.dart
+++ lib/widgets/home_feed_item_widget.dart
@@ -260,7 +260,7 @@ class _HomeFeedItemWidgetState extends State<HomeFeedItemWidget> {
                           CommonSnackBar.show(
                             context: context,
                             message: '본인 게시글에는 좋아요를 누를 수 없습니다.',
-                            type: SnackBarType.info,
+                            type: SnackBarType.error,
                           );
                         }
                         return;
