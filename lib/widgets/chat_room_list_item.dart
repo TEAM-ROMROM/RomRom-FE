463039df9e0ad3dc75a1877bbd36fa22c4992ef8
Lee seohuyn <leesophy0805@gmail.com>
2026-02-10 18:55:17 +0900

    채팅 리스트 사진 썸네일 추가 : feat : ChatRoomDetailDto 에 targetItemImageUrl 변수 추가, 채팅방 목록에 상대방물건 사진 썸네일 추가 https://github.com/TEAM-ROMROM/RomRom-FE/issues/478

--- lib/widgets/chat_room_list_item.dart
+++ lib/widgets/chat_room_list_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:romrom_fe/models/app_colors.dart';
 import 'package:romrom_fe/models/app_theme.dart';
+import 'package:romrom_fe/widgets/common/cached_image.dart';
 import 'package:romrom_fe/widgets/user_profile_circular_avatar.dart';
 
 /// 채팅방 리스트 아이템 위젯
@@ -12,6 +13,7 @@ class ChatRoomListItem extends StatelessWidget {
   final String location;
   final String timeAgo;
   final String messagePreview;
+  final String? targetItemImageUrl;
   final int unreadCount;
   final bool isNew;
   final VoidCallback onTap;
@@ -25,6 +27,7 @@ class ChatRoomListItem extends StatelessWidget {
     required this.location,
     required this.timeAgo,
     required this.messagePreview,
+    this.targetItemImageUrl,
     this.unreadCount = 0,
     this.isNew = false,
     required this.onTap,
@@ -50,7 +53,7 @@ class ChatRoomListItem extends StatelessWidget {
               ),
             ),
 
-            SizedBox(width: 12.w),
+            SizedBox(width: 16.w),
 
             // 2. 중앙 정보 영역
             Expanded(
@@ -121,22 +124,39 @@ class ChatRoomListItem extends StatelessWidget {
                       ),
                       // 3. 읽지 않은 메시지 뱃지
                       if (unreadCount > 0) ...[
-                        SizedBox(width: 8.w),
-                        Container(
-                          width: 16.w,
-                          height: 16.h,
-                          decoration: const BoxDecoration(shape: BoxShape.circle, color: AppColors.chatUnreadBadge),
-                          alignment: Alignment.center,
-                          child: Text(
-                            // 99 초과 시 '99+' 표시
-                            unreadCount > 99 ? '99+' : '$unreadCount',
-                            style: CustomTextStyles.p3.copyWith(color: Colors.white, fontWeight: FontWeight.w500),
+                        Padding(
+                          padding: EdgeInsets.only(right: 10.0.w, left: 8.w),
+                          child: Container(
+                            width: 16.w,
+                            height: 16.h,
+                            decoration: const BoxDecoration(shape: BoxShape.circle, color: AppColors.chatUnreadBadge),
+                            alignment: Alignment.center,
+                            child: Text(
+                              // 99 초과 시 '99+' 표시
+                              unreadCount > 99 ? '99+' : '$unreadCount',
+                              style: CustomTextStyles.p3.copyWith(color: Colors.white, fontWeight: FontWeight.w500),
+                            ),
                           ),
                         ),
                       ],
                     ],
                   ),
                 ],
+              ),
+            ),
+            SizedBox(
+              width: 40.w,
+              height: 40.w,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.r),
+                child: targetItemImageUrl != null
+                    ? CachedImage(
+                        imageUrl: targetItemImageUrl!,
+                        fit: BoxFit.cover,
+
+                        errorWidget: Container(color: AppColors.imagePlaceholderBackground),
+                      )
+                    : Container(color: AppColors.imagePlaceholderBackground),
               ),
             ),
           ],
