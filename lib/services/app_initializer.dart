5aae855da63f8addbb42cc2f85f8542c67d5f07e
Lee seohuyn <leesophy0805@gmail.com>
2026-03-09 23:06:07 +0900

    구글 로그인 튕기는 오류 : feat : 구글로그인 초기화 로직 추가 https://github.com/TEAM-ROMROM/RomRom-FE/issues/634

--- lib/services/app_initializer.dart
+++ lib/services/app_initializer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:logging/logging.dart';
 
@@ -8,6 +9,7 @@ import 'package:logging/logging.dart';
 Future<void> initialize() async {
   await loadEnv(); // .env 파일 로딩
   await initNaverMap(); // 네이버 지도 초기화
+  await initGoogleSignIn(); // Google Sign-In 초기화 (v7 필수)
   initKakaoSdk(); // 카카오 sdk 초기화
   initLogger(); // logger 초기화
 }
@@ -21,6 +23,17 @@ Future<void> loadEnv() async {
 Future<void> initNaverMap() async {
   final flutterNaverMap = FlutterNaverMap();
   await flutterNaverMap.init(clientId: dotenv.get('NMF_CLIENT_ID'), onAuthFailed: (ex) => Logger("네이버맵 인증오류"));
+}
+
+/// Google Sign-In 초기화 (google_sign_in v7 필수)
+/// Android: serverClientId (web client ID) 없으면 clientConfigurationError 발생
+Future<void> initGoogleSignIn() async {
+  try {
+    await GoogleSignIn.instance.initialize(serverClientId: dotenv.get('GOOGLE_SERVER_CLIENT_ID'));
+    debugPrint('[AppInit] Google Sign-In 초기화 완료');
+  } catch (e) {
+    debugPrint('[AppInit] Google Sign-In 초기화 실패: $e');
+  }
 }
 
 /// 카카오 SDK 초기화
