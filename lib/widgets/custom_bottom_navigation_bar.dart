ccdcb7f6db15cb062a44616ef804d161e4dc9770
SUH SAECHAN <chan4760@gmail.com>
2026-03-18 00:13:26 +0900

    iPad에서 하단 메뉴바(BottomNavigationBar)가 잘리는 현상 : fix : ipad 화면 높이에서 과도하게 확대되는 문제를 고정 픽셀값으로 교체하여 해결 https://github.com/TEAM-ROMROM/RomRom-FE/issues/677

--- lib/widgets/custom_bottom_navigation_bar.dart
+++ lib/widgets/custom_bottom_navigation_bar.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:romrom_fe/enums/navigation_tab_items.dart';
 import 'package:romrom_fe/models/app_colors.dart';
 import 'package:romrom_fe/models/app_theme.dart';
@@ -14,7 +13,7 @@ class CustomBottomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double bottomPadding = Platform.isIOS ? 52.h + MediaQuery.of(context).padding.bottom : 70.h;
+    double bottomPadding = Platform.isIOS ? 58 + MediaQuery.of(context).padding.bottom : 70;
     return Container(
       width: MediaQuery.of(context).size.width,
       // 기본 높이 + 시스템 네비게이션 영역 패딩
@@ -40,16 +39,16 @@ class CustomBottomNavigationBar extends StatelessWidget {
       child: SizedBox(
         width: MediaQuery.of(context).size.width / 5,
         child: Padding(
-          padding: EdgeInsets.only(top: 12.h),
+          padding: const EdgeInsets.only(top: 12),
           child: Column(
             children: [
               // 아이콘 (선택됨: 흰색, 선택 안됨: 회색)
               Icon(
                 icon,
                 color: isSelected ? AppColors.textColorWhite : AppColors.bottomNavigationDisableIcon,
-                size: 24.sp,
+                size: 24,
               ),
-              SizedBox(height: 8.h), // 아이콘과 텍스트 사이 간격
+              const SizedBox(height: 8), // 아이콘과 텍스트 사이 간격
               // 텍스트 (선택됨: 흰색, 선택 안됨: 회색)
               Text(
                 label,
