914e34b60abbaafbb1e72f2a0a19a253416e325f
Lee seohuyn <leesophy0805@gmail.com>
2026-04-13 16:14:09 +0900

    공유 버튼 클릭 시 Firebase 딥링크로 앱/스토어 분기 이동 : feat : 불필요한 import 삭제 https://github.com/TEAM-ROMROM/RomRom-FE/issues/765

--- lib/utils/common_utils.dart
+++ lib/utils/common_utils.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
-import 'dart:ui';
 import 'package:romrom_fe/enums/navigation_types.dart';
 import 'package:romrom_fe/models/app_colors.dart';
 import 'package:romrom_fe/models/app_urls.dart';
