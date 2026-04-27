fd36a9344e08b027d251630a0320db13cf77db38
Lee seohuyn <leesophy0805@gmail.com>
2026-04-27 20:40:55 +0900

    카테고리 칩 선택페이지 ui 개선 : feat : 기존 카테고리 칩에서 아이콘 있는 칩으로 변경  https://github.com/TEAM-ROMROM/RomRom-FE/issues/807

--- lib/widgets/common/category_chip.dart
+++ lib/widgets/common/category_chip.dart
@@ -1,41 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:romrom_fe/models/app_colors.dart';
 import 'package:romrom_fe/models/app_theme.dart';
+import 'package:romrom_fe/widgets/common/app_pressable.dart';
 
 class CategoryChip extends StatelessWidget {
   final String label;
   final bool isSelected;
   final VoidCallback onTap;
+  final String iconPath;
 
-  const CategoryChip({super.key, required this.label, required this.isSelected, required this.onTap});
+  const CategoryChip({
+    super.key,
+    required this.label,
+    required this.isSelected,
+    required this.onTap,
+    required this.iconPath,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return RawChip(
-      label: Text(
-        label,
-        style: CustomTextStyles.p2.copyWith(
-          fontSize: 14.0.w,
-          color: isSelected ? AppColors.textColorBlack : AppColors.textColorWhite,
-          wordSpacing: -0.32.w,
+    return Column(
+      children: [
+        AppPressable(
+          onTap: onTap,
+          borderRadius: BorderRadius.circular(10.r),
+          child: Container(
+            height: 80.w,
+            width: 80.w,
+            decoration: BoxDecoration(
+              color: isSelected ? AppColors.categoryChipSelected : AppColors.categoryChipUnselected,
+              borderRadius: BorderRadius.circular(10.r),
+              border: Border.all(
+                color: isSelected ? AppColors.categoryChipBorder : AppColors.transparent,
+                width: 1.0.w,
+              ),
+            ),
+            child: Center(
+              child: SvgPicture.asset(iconPath, width: 40.w, height: 40.w),
+            ),
+          ),
         ),
-      ),
-      labelPadding: EdgeInsets.zero,
-      padding: EdgeInsets.symmetric(horizontal: 16.0.w, vertical: 10.0.h),
-      selected: isSelected,
-      selectedColor: AppColors.primaryYellow,
-      backgroundColor: AppColors.primaryBlack,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadiusDirectional.circular(100.r)),
-      side: BorderSide(
-        color: isSelected ? AppColors.primaryYellow : AppColors.textColorWhite,
-        strokeAlign: BorderSide.strokeAlignOutside,
-        width: 1.0.w,
-      ),
-      checkmarkColor: Colors.transparent,
-      showCheckmark: false,
-      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-      onSelected: (_) => onTap(),
+        SizedBox(height: 8.h),
+        Text(label, style: CustomTextStyles.p2),
+      ],
     );
   }
 }
